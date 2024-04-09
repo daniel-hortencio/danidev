002a2459082b232ae1a49c4f9703a2b6847c8df2
daniel-hortencio <hortencio.daniel@gmail.com>
2024-04-09 18:33:56 -0300

    adjusts

--- ui/Icons/PhosphorIcon/index.tsx
+++ ui/Icons/PhosphorIcon/index.tsx
@@ -1,17 +1,15 @@
 import * as React from "react";
 import * as PhosphorIcons from "@phosphor-icons/react";
 
-export type PhosphorIconProps = {
-  name: PhosphorIconName;
-  size?: number;
-  color?: string;
-};
-
 export const PhosphorIcon = ({
   name,
   size = 20,
   color = "",
-}: PhosphorIconProps) => {
+}: {
+  name: PhosphorIconName;
+  size?: number;
+  color?: string;
+}) => {
   const PhIcon: any =
     PhosphorIcons[name as keyof typeof PhosphorIcons] || PhosphorIcons.XSquare;
 
